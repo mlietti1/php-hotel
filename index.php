a498a8f111fd437143d1105ce061432031d1baa5
mlietti1 <mcf.lietti@outlook.com>
2022-12-02 10:44:52 +0100

    correzione

--- index.php
+++ index.php
@@ -40,6 +40,50 @@ $hotels = [
 
 ];
 
+// logica per filtrare gli hotel
+
+// di default elenco da ciclare è tutto l'array
+$selected_hotels = $hotels;
+
+var_dump($_GET);
+
+// vedo se dato esiste in get parking, non isset ma !empty, perchè verifica sia se esiste sia se è vuoto
+
+if (!empty($_GET['parking'])) {
+  //creo array dove pushare hotel filtrati
+  $temp_hotels = [];
+
+  // ciclo array hotels e pusho nel nuovo array solo hotel con parking
+  foreach ($selected_hotels as $hotel) {
+    if ($hotel['parking']) $temp_hotels[] = $hotel;
+  }
+  // sostityisco array selected con array temporaneo
+
+  $selected_hotels = $temp_hotels;
+}
+
+if (isset($_GET['parking']) && empty($_GET['parking'])) {
+  //creo array dove pushare hotel filtrati
+  $temp_hotels = [];
+
+  // ciclo array hotels e pusho nel nuovo array solo hotel con parking
+  foreach ($selected_hotels as $hotel) {
+    if (!$hotel['parking']) $temp_hotels[] = $hotel;
+  }
+  // sostityisco array selected con array temporaneo
+
+  $selected_hotels = $temp_hotels;
+}
+
+if (!empty($_GET['vote'])) {
+  $temp_hotels = [];
+  foreach ($selected_hotels as $hotel) {
+    if ($hotel['vote'] >= $_GET['vote']) $temp_hotels[] = $hotel;
+  }
+
+  $selected_hotels = $temp_hotels;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -57,6 +101,37 @@ $hotels = [
 
   <div class="container">
     <h1 class="my-5">Hotel PHP</h1>
+
+    <form class="d-flex align-items-center" action="./index.php" method="GET">
+
+      <div class="form-check me-3">
+        <input class="form-check-input" type="radio" name="parking" id="noparking" value="">
+        <label class="form-check-label" for="noparking">
+          No parking
+        </label>
+      </div>
+
+      <div class="form-check me-3">
+        <input class="form-check-input" type="radio" name="parking" id="withparking" value="1">
+        <label class="form-check-label" for="withparking">
+          With parking
+        </label>
+      </div>
+
+      <select class="form-select me-2 w-25" aria-label="Default select example" name="vote">
+        <option selected value="">Select rating</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+
+      <button class="btn btn-warning me-2" type="submit">Apply</button>
+      <button class="btn btn-secondary" type="reset">Reset</button>
+
+    </form>
+
     <table class="my-5 table table-striped">
       <thead>
         <tr>
@@ -68,19 +143,23 @@ $hotels = [
         </tr>
       </thead>
       <tbody>
-        <?php foreach ($hotels as $hotel) : ?>
+        <?php foreach ($selected_hotels as $hotel) : ?>
           <tr>
-            <?php foreach ($hotel as $key => $value) : ?>
-              <td>
-                <?php if ($key === 'parking' && $value) : ?>
-                  <?php echo 'Yes' ?>
-                <?php elseif ($key === 'parking' && !$value) : ?>
-                  <?php echo 'No' ?>
-                <?php else : ?>
-                  <?php echo $value ?>
-                <?php endif ?>
-              </td>
-            <?php endforeach ?>
+            <td>
+              <?php echo $hotel['name'] ?>
+            </td>
+            <td>
+              <?php echo $hotel['description'] ?>
+            </td>
+            <td>
+              <?php echo $hotel['parking'] ? 'Yes' : 'No' ?>
+            </td>
+            <td>
+              <?php echo $hotel['vote'] ?>
+            </td>
+            <td>
+              <?php echo $hotel['distance_to_center'] ?>
+            </td>
           </tr>
         <?php endforeach ?>
       </tbody>
